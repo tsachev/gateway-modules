4f77e1deac3e394e8100eaf164580d9ec8a6ef7f
Lyubomir Konstantinov <lubomir.konstantinov@tick42.com>
2020-09-01 12:07:33 +0300

    - new development version

--- metrics-core/project.clj
+++ metrics-core/project.clj
@@ -1,4 +1,4 @@
-(defproject com.tick42.gateway/metrics-core "3.0.9"
+(defproject com.tick42.gateway/metrics-core "3.0.10-SNAPSHOT"
   :plugins [[lein-modules "0.3.11"]]
   :dependencies [[com.taoensso/timbre "_"]
                  [org.clojure/core.async "_"]])
