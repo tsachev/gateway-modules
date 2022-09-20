6e1d304b896581ea45fbd22d01d3a2f4f6861c97
vtsanev <vladimir.tsanev@tick42.com>
2022-09-20 18:01:55 +0300

    Version 3.0.18

--- common/project.clj
+++ common/project.clj
@@ -1,4 +1,4 @@
-(defproject com.tick42.gateway/common "3.0.18-SNAPSHOT"
+(defproject com.tick42.gateway/common "3.0.18"
   :plugins [[lein-modules "0.3.11"]]
 
   :dependencies [[instaparse "_"]
--- metrics-core/project.clj
+++ metrics-core/project.clj
@@ -1,4 +1,4 @@
-(defproject com.tick42.gateway/metrics-core "3.0.18-SNAPSHOT"
+(defproject com.tick42.gateway/metrics-core "3.0.18"
   :plugins [[lein-modules "0.3.11"]]
   :dependencies [[com.taoensso/timbre "_"]
                  [org.clojure/core.async "_"]])
